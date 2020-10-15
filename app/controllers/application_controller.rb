eb23de80c2f42a5f6f44055cc8abc9713a7531d5
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-10-15 06:13:51 +0000

    square works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
 
   def blank_square_form
-    
     render({ :template => "calculation_templates/square_form.html.erb"})
-
   end
 
+  def calculate_square
+    # params = {'number'=>n}
+    @num = params.fetch('number').to_f
+    @square_num = @num**2
+    render({ :template => "calculation_templates/square_results.html.erb"})
+  end
 end
