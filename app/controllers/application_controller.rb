886673589052cba64bb9107bbb6f3c1b30634498
KevinDelaFuente <kevin.delafuente@chicagobooth.edu>
2020-10-15 14:55:52 +0000

    square root success

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,17 @@ class ApplicationController < ActionController::Base
     render({ :template => "calculation_templates/square_results.html.erb"})
   end
 
+  def blank_square_root_form
+    render({ :template => "calculation_templates/square_root_form.html.erb"})
+  end
+
+  def calculate_square_root
+      # params = {'number'=>n}
+    @num = params.fetch('number').to_f
+    @square_root = @num**0.5
+    render({ :template => "calculation_templates/square_root_results.html.erb"})
+  end  
+
   def calculate_random
     @lower = params.fetch('user_min').to_f
     @upper = params.fetch('user_max').to_f
